19b610bb5762776e6e4d992fdccbe0f9b497c777
Yili0616 <yilihui@gatech.edu>
2018-03-31 16:36:18 -0400

    lauch the ball when hit ball laucher

---
+++ TrainingMode/TrainingTutorial1.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { Font } from 'expo';
+import { TouchableOpacity, StyleSheet, View, Image } from 'react-native';
+import { Container, Content, Left, Right, Text, ListItem, Radio } from 'native-base';
+
+import Button from '../Components/Button';
+import Navbar from '../Components/Navbar';
+import Hidden from '../Components/Hidden';
+
+export default class TrainingTutorial1 extends Component {
+  static navigationOptions = {
+    drawerLabel: <Hidden />,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontLoaded: false,
+    };
+  }
+
+
+  async componentDidMount() {
+    await Expo.Font.loadAsync({
+      'bungee-inline': require('../assets/fonts/BungeeInline-Regular.ttf'),
+      'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
+      'Roboto_medium': require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ fontLoaded: true });
+  }
+
+
+  render() {
+    const { navigation } = this.props;
+    if (!this.state.fontLoaded) { return null;}
+
+    return (
+      <Container style={styles.container}>
+
+        <View>
+          <Image style={styles.court}
+            source={require('../assets/images/TrainingTut1.png')}/>
+
+          <Button style={styles.button}
+           label='Next'
+           onPress={() => this.props.navigation.navigate("TrainingTutorial2")}/>
+
+        </View>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#2A5D38',
+  },
+  button: {
+    backgroundColor: '#ffffff',
+    position: 'absolute',
+    zIndex: 3,
+    bottom: 40,
+    left: 60
+  },
+  court: {
+    height: '100%',
+    alignSelf: 'center',
+    resizeMode: 'contain',
+    zIndex: 0,
+  },
+});
